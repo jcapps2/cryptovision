464093e945832bc84ef52ce1712ae92c8d16211c
Jacob Capps <jcapps1@Jacobs-MacBook-Air.local>
2020-08-20 11:45:14 -0500

    Add Nav with grid

--- src/App/index.js
+++ src/App/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import WelcomeMessage from "./WelcomeMessage";
 import Layout from "./Layout";
+import Nav from "./Nav";
 
 function App() {
   return (
     <Layout>
+      <Nav />
       <WelcomeMessage />
     </Layout>
   );
